aa3f42f5cd849da3606108dad5bbeb27d587b51c
Phuong Uyen Tran <2990288t@student.gla.ac.uk>
2025-06-26 17:38:53 +0100

    Initial commit

---
+++ backend/db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// Test DB connection (optional)
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
+  }
+  client.query('SELECT NOW()', (err, result) => {
+    release();
+    if (err) {
+      return console.error('Error executing query', err.stack);
+    }
+    console.log('Database connected:', result.rows);
+  });
+});
+
+module.exports = pool;
---
+++ backend/index.js
@@ -0,0 +1,34 @@
+// Load environment variables from .env
+require('dotenv').config();
+// console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
+const pool = require('./db');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // for parsing JSON requests
+
+// Simple test route
+app.get('/', (req, res) => {
+  res.send('TradeWise backend server is running');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+// app.get('/time', async (req, res) => {
+//   try {
+//     const result = await pool.query('SELECT NOW()');
+//     res.json(result.rows[0]);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send('Database error');
+//   }
+// });
