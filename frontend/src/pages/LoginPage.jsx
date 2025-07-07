187230d021184e148b5f56242d0dc5d8a5879028
Phuong Uyen Tran <2990288t@student.gla.ac.uk>
2025-07-07 19:46:25 +0100

    Set up React Router

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,3 @@
+export default function LoginPage() {
+  return <h1>Welcome to TradeWise</h1>;
+}
---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,3 @@
+export default function RegisterPage() {
+  return <h1>Welcome to TradeWise</h1>;
+}
